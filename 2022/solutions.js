cf025387bbe3b615518dc34a69cfcb968f24aa0b
Lukas Bangerter <lulukas.klier@gmail.com>
2024-11-30 13:32:44 +0100

    solve 2022 problem

--- 2022/solutions.js
+++ 2022/solutions.js
@@ -1,10 +1,11 @@
 import { getHighestCalorieCount, getHighestCalorieCounts } from './01/index.js'
 import { getTotalScoreStrategy1, getTotalScoreStrategy2 } from './02/index.js'
+import { getPrioritiesOfSameItems, getPrioritiesOfSameItems2 } from './03/index.js'
 
 export const solutions = {
   '01': [getHighestCalorieCount, getHighestCalorieCounts],
   '02': [getTotalScoreStrategy1, getTotalScoreStrategy2],
-  '03': [],
+  '03': [getPrioritiesOfSameItems, getPrioritiesOfSameItems2],
   '04': [],
   '05': [],
   '06': [],
