3d2fe08bb5bbdb3ff2e5ebde1d6b53b6407e57c3
Lukas Bangerter <lulukas.klier@gmail.com>
2024-12-02 06:48:01 +0100

    Solve 02 2024

--- 2024/02/index.js
+++ 2024/02/index.js
@@ -4,6 +4,34 @@ export const getPart1 = (filePath) => {
   const lines = getLines(filePath)
 
   let result = 0
+  lines.forEach((line) => {
+    const levels = line.split(' ').map((x) => +x)
+    let isSave = levels[0] !== levels[1]
+
+    if (levels[0] < levels[1]) {
+      let l = 1
+      while (l < levels.length && isSave === true) {
+        if (levels[l - 1] >= levels[l] || levels[l] - levels[l - 1] > 3) {
+          isSave = false
+        }
+        l++
+      }
+    }
+
+    if (levels[0] > levels[1]) {
+      let l = 1
+      while (l < levels.length && isSave === true) {
+        if (levels[l - 1] <= levels[l] || levels[l - 1] - levels[l] > 3) {
+          isSave = false
+        }
+        l++
+      }
+    }
+
+    if (isSave === true) {
+      result++
+    }
+  })
 
   return result
 }
@@ -12,6 +40,41 @@ export const getPart2 = (filePath) => {
   const lines = getLines(filePath)
 
   let result = 0
+
+  lines.forEach((line) => {
+    const levels = line.split(' ').map((x) => +x)
+    let isSave = false
+    let drop = 0
+
+    while (drop < levels.length && isSave === false) {
+      const shortLevels = levels.filter((l, i) => i !== drop)
+      isSave = shortLevels[0] !== shortLevels[1]
+      if (shortLevels[0] < shortLevels[1]) {
+        let l = 1
+        while (l < shortLevels.length && isSave === true) {
+          if (shortLevels[l - 1] >= shortLevels[l] || shortLevels[l] - shortLevels[l - 1] > 3) {
+            isSave = false
+          }
+          l++
+        }
+      }
+
+      if (shortLevels[0] > shortLevels[1]) {
+        let l = 1
+        while (l < shortLevels.length && isSave === true) {
+          if (shortLevels[l - 1] <= shortLevels[l] || shortLevels[l - 1] - shortLevels[l] > 3) {
+            isSave = false
+          }
+          l++
+        }
+      }
+      drop++
+    }
+
+    if (isSave === true) {
+      result++
+    }
+  })
 
   return result
 }
