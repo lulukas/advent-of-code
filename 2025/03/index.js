c1517137baf739e18cb9bc9ba4ae50581f199ad5
Lukas Bangerter <lulukas.klier@gmail.com>
2025-12-03 19:33:04 +0100

    Day 03

--- 2025/03/index.js
+++ 2025/03/index.js
@@ -1,15 +1,44 @@
 import { getLines } from '../../utils/fileStreamUtils.js'
 
 export const getPart1 = (filePath) => {
-  const lines = getLines(filePath)
+  const banks = getLines(filePath)
   let result = 0
+
+  banks.forEach((bank) => {
+    result += getHighestJoltage(bank.split('').map(Number), 2)
+  })
 
   return result
 }
 
 export const getPart2 = (filePath) => {
-  const lines = getLines(filePath)
+  const banks = getLines(filePath)
   let result = 0
 
+  banks.forEach((bank) => {
+    result += getHighestJoltage(bank.split('').map(Number), 12)
+  })
+
   return result
+}
+
+const getHighestJoltage = (batteries, count) => {
+  const highestJoltage = []
+  let i = 0
+
+  for (let x = 0; x < count; x++) {
+    let higestDigit = [0, 0]
+
+    while (i < batteries.length + x - (count - 1) && higestDigit[0] < 9) {
+      if (batteries[i] > higestDigit[0]) {
+        higestDigit = [batteries[i], i]
+      }
+      i++
+    }
+
+    highestJoltage.push(higestDigit[0])
+    i = higestDigit[1] + 1
+  }
+
+  return +highestJoltage.join('')
 }
